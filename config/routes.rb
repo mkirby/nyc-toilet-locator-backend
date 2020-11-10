58db13638c9943e0859799a488735915b1c46d11
bryanmoon1991 <bryanmoon1991@gmail.com>
2020-11-10 15:39:49 -0500

    added working pagination

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # resources :reviews
   # resources :toilets, only: [:index]
-
+  
   namespace :api do
     namespace :v1 do
       resources :toilets, only: [:show, :index]
+      get 'info', action: :info, controller: 'toilets'
     end
   end
 end
