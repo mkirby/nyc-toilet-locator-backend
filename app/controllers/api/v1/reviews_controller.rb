7c1f29eaadda3e965275f2290fc4ebff4f102248
Matthew Kirby <matt.kirby@yahoo.com>
2020-11-10 17:18:34 -0500

    added review api routees

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::ReviewsController < ApplicationController
+
+  def show
+    review = Review.find(params[:id])
+    render json: review
+  end
+
+  def index
+      reviews = Review.all
+      render json: reviews
+  end
 end
