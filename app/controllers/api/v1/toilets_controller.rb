969e38dc227c06e9a9be7a3bd33f496a10d6c52c
Matthew Kirby <matt.kirby@yahoo.com>
2020-11-13 13:18:16 -0500

    merged filter-near-me into index method

--- app/controllers/api/v1/toilets_controller.rb
+++ app/controllers/api/v1/toilets_controller.rb
@@ -10,6 +10,9 @@ class Api::V1::ToiletsController < ApplicationController
             toilets = Toilet.search(params[:query])
             neighborhoods = toilets.map{|toilet| toilet.neighborhood}.uniq.sort
             toilets = toilets.paginate(page: params[:page], per_page: 8)
+        elsif params[:lat]
+            toilets = Toilet.near([params[:lat], params[:long]], 1).paginate(page: params[:page], per_page: 8)
+            neighborhoods = Toilet.all.map{|toilet| toilet.neighborhood}.uniq.sort
         else
             toilets = Toilet.paginate(page: params[:page], per_page: 8)
             neighborhoods = Toilet.all.map{|toilet| toilet.neighborhood}.uniq.sort
@@ -28,14 +31,6 @@ class Api::V1::ToiletsController < ApplicationController
         toilet = Toilet.find(params[:id])
         toilet.update(toiletParams)
         render json: toilet
-    end
-
-    def toilets_near_user
-        toilets = Toilet.near([params[:lat], params[:long]], 1).paginate(page: params[:page], per_page: 8)
-        neighborhoods = Toilet.all.map{|toilet| toilet.neighborhood}.uniq.sort
-        lastPage = toilets.total_pages
-        serialized_toilets = toilets.map { |t| ToiletSerializer.new(t) }
-        render json: {toilets: serialized_toilets, lastPage: lastPage, neighborhoods: neighborhoods}
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :toilets, only: [:show, :index, :create, :update]
       resources :reviews, only: [:show, :index, :create, :destroy]
-      get 'near_user', action: :toilets_near_user, controller: 'toilets'
     end
   end
 end
